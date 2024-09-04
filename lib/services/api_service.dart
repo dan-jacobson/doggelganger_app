6a0bff2a253082b3bcfd69c4e5fc492ec146159c
Dan Jacobson <danrowejacobson@gmail.com>
2024-09-04 18:43:42 -0400

    feat: Change API endpoint from /match to /embed

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,14 +6,15 @@ class ApiService {
   static const String baseUrl = 'http://0.0.0.0:8000'; // FastAPI server running locally
 
   static Future<DogData> uploadImageAndGetMatch(String imagePath) async {
-    var request = http.MultipartRequest('POST', Uri.parse('$baseUrl/match'));
+    var request = http.MultipartRequest('POST', Uri.parse('$baseUrl/embed'));
     request.files.add(await http.MultipartFile.fromPath('image', imagePath));
 
     var response = await request.send();
     if (response.statusCode == 200) {
       var responseData = await response.stream.bytesToString();
       var decodedData = json.decode(responseData);
-      return DogData.fromJson(decodedData);
+      var bestResponse = decodedData['similar_images'][0];
+      return DogData.fromJson(bestResponse['metadata']);
     } else {
       throw Exception('Failed to get a match. Status code: ${response.statusCode}');
     }
