ab789dfe07ac07171b9aeb47503d756c70f2e8db
Dan Jacobson <danrowejacobson@gmail.com>
2024-12-16 12:24:27 -0500

    feat: Update DogData model

--- lib/models/dog_data.dart
+++ lib/models/dog_data.dart
@@ -1,42 +1,39 @@
+import 'dart:ffi';
+
 class DogData {
   final String name;
-  final String fullName;
-  final String location;
-  final String imageName;
-  final String? imageUrl;
+  final Map location;
   final String age;
   final String sex;
   final String breed;
-  final String distance;
-  final String adoptionLink;
+  final String url;
+  final String photo;
+  final String croppedPhoto;
 
   DogData({
     required this.name,
-    required this.fullName,
     required this.location,
-    required this.imageName,
-    this.imageUrl,
     required this.age,
     required this.sex,
     required this.breed,
-    required this.distance,
-    required this.adoptionLink,
+    required this.url,
+    required this.photo,
+    required this.croppedPhoto,
   });
 
   factory DogData.fromJson(Map<String, dynamic> json) {
     return DogData(
       name: json['name'],
-      fullName: json['full_name'],
       location: json['location'],
-      imageName: json['local_image']?.replaceAll('.jpg', '') ?? '',
-      imageUrl: json['image_url'],
       age: json['age'],
       sex: json['sex'],
       breed: json['breed'],
-      distance: json['distance'],
-      adoptionLink: json['adoption_link'],
+      url: json['url'],
+      photo: json['primary_photo'],
+      croppedPhoto: json['primary_photo_cropped'],
+      similarity: json['similarity']
     );
   }
 
-  String get imageSource => imageUrl ?? 'assets/images/carousel/$imageName.jpg';
+  // String get imageSource => photo ?? 'assets/images/carousel/$imageName.jpg';
 }
