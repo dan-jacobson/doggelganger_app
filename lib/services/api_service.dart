1723d9373c7b3c9ed4ea56dcb53cf86cd45a2a0c
Dan Jacobson <danrowejacobson@gmail.com>
2025-01-06 22:54:10 -0500

    feat: Add logging and error handling to API service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,22 +7,35 @@ class ApiService {
   static String get baseUrl => Environment.apiUrl;
 
   static Future<DogData> uploadImageAndGetMatch(String imagePath) async {
+    try {
     var request = http.MultipartRequest('POST', Uri.parse('$baseUrl/embed'));
     request.files.add(await http.MultipartFile.fromPath('image', imagePath));
 
-    var response = await request.send();
-    if (response.statusCode == 200) {
-      var responseData = await response.stream.bytesToString();
-      var decodedData = json.decode(responseData);
+    print('Sending request to: ${request.url}');                                                                                                          
+    print('Request headers: ${request.headers}');                                                                                                         
+    print('Request fields: ${request.fields}');
 
-      if (decodedData.containsKey("result")) {
-        return DogData.fromJson(decodedData['result']);
-      } else {
-        throw Exception('No similar images found in the response');
-      }
-    } else {
-      throw Exception(
-          'Failed to get a match. Status code: ${response.statusCode}');
-    }
-  }
+    var response = await request.send();                                                                                                                  
+    var responseBody = await response.stream.bytesToString();                                                                                             
+                                                                                                                                                          
+    print('Response status code: ${response.statusCode}');                                                                                                
+    print('Response body: $responseBody');                                                                                                                
+                                                                                                                                                          
+    if (response.statusCode == 200) {                                                                                                                     
+      var decodedData = json.decode(responseBody);                                                                                                        
+                                                                                                                                                          
+      if (decodedData.containsKey("result")) {                                                                                                            
+        return DogData.fromJson(decodedData['result']);                                                                                                   
+      } else {                                                                                                                                            
+        throw Exception('No similar images found in the response');                                                                                       
+      }                                                                                                                                                   
+    } else {                                                                                                                                              
+      throw Exception(                                                                                                                                    
+          'Failed to get a match. Status code: ${response.statusCode}. Response: $responseBody');                                                         
+    }                                                                                                                                                     
+  } catch (e) {                                                                                                                                           
+    print('Error in uploadImageAndGetMatch: $e');                                                                                                         
+    rethrow;                                                                                                                                              
+  }                                                                                                                                                       
+}                                                                                                                                                         
 }
