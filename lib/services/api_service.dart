43edfe17256f0bf6fbe6ed6ac6167e1f437117af
Dan Jacobson <danrowejacobson@gmail.com>
2025-01-10 14:29:22 -0500

    fix(api): try and coerce file to be a jpg

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import 'package:doggelganger_app/models/dog_data.dart';
 import 'package:doggelganger_app/config/environment.dart';
 
@@ -7,35 +8,24 @@ class ApiService {
   static String get baseUrl => Environment.apiUrl;
 
   static Future<DogData> uploadImageAndGetMatch(String imagePath) async {
-    try {
     var request = http.MultipartRequest('POST', Uri.parse('$baseUrl/embed'));
-    request.files.add(await http.MultipartFile.fromPath('image', imagePath));
+    request.files.add(await http.MultipartFile.fromPath('image', imagePath,
+        // TODO(drj): is it always a jpg?
+        contentType: MediaType('image', 'jpeg')));
 
-    print('Sending request to: ${request.url}');                                                                                                          
-    print('Request headers: ${request.headers}');                                                                                                         
-    print('Request fields: ${request.fields}');
+    var response = await request.send();
+    var responseBody = await response.stream.bytesToString();
 
-    var response = await request.send();                                                                                                                  
-    var responseBody = await response.stream.bytesToString();                                                                                             
-                                                                                                                                                          
-    print('Response status code: ${response.statusCode}');                                                                                                
-    print('Response body: $responseBody');                                                                                                                
-                                                                                                                                                          
-    if (response.statusCode == 200) {                                                                                                                     
-      var decodedData = json.decode(responseBody);                                                                                                        
-                                                                                                                                                          
-      if (decodedData.containsKey("result")) {                                                                                                            
-        return DogData.fromJson(decodedData['result']);                                                                                                   
-      } else {                                                                                                                                            
-        throw Exception('No similar images found in the response');                                                                                       
-      }                                                                                                                                                   
-    } else {                                                                                                                                              
-      throw Exception(                                                                                                                                    
-          'Failed to get a match. Status code: ${response.statusCode}. Response: $responseBody');                                                         
-    }                                                                                                                                                     
-  } catch (e) {                                                                                                                                           
-    print('Error in uploadImageAndGetMatch: $e');                                                                                                         
-    rethrow;                                                                                                                                              
-  }                                                                                                                                                       
-}                                                                                                                                                         
+    if (response.statusCode == 200) {
+      var decodedData = json.decode(responseBody);
+      if (decodedData.containsKey("result")) {
+        return DogData.fromJson(decodedData['result']);
+      } else {
+        throw Exception('API returned empty result.');
+      }
+    } else {
+      throw Exception(
+          'Failed to get a match. Status code: ${response.statusCode}. Response: $responseBody');
+    }
+  }
 }
